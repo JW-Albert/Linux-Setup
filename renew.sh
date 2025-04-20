0cefef3bbd880ea254c2c893de00b25a9565d7c2
jw-albert <ru04jo30801@gmail.com>
2025-04-20 19:54:10 +0800

    Update renew.sh

--- renew.sh
+++ renew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update -y && apt upgrade -y && \
-apt install sudo ufw wget curl -y && \
+apt install sudo ufw wget curl systemd-timesyncd -y && \
 ufw allow 22/tcp && \
 #ufw allow 55555/tcp && \
 ufw default deny incoming && \
@@ -9,6 +9,7 @@ ufw enable && \
 ufw status && \
 echo "UFW turn on!" && \
 timedatectl set-timezone Asia/Taipei && \
+systemctl enable systemd-timesyncd --now && \
 timedatectl set-ntp true && \
 hwclock --systohc && \
 hwclock --show && \

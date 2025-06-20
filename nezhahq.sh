ec309fa00707ac8a5fc7b3ce88db9960faab7a19
jw-albert <ru04jo30801@gmail.com>
2025-06-21 01:42:22 +0800

    add apt

--- nezhahq.sh
+++ nezhahq.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # This script needs to be executed as root!
 
+apt install curl unzip -y
+
 curl -L https://raw.githubusercontent.com/nezhahq/scripts/main/agent/install.sh -o agent.sh && chmod +x agent.sh && env NZ_SERVER=107.173.19.61:8008 NZ_TLS=false NZ_CLIENT_SECRET=dgNWh5mZsk6zLOjzMuX0BsHCM9JXggYB ./agent.sh

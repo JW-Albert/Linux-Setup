42031b820a31fe734d84f061351b6f2802192cf0
jw-albert <ru04jo30801@gmail.com>
2025-09-19 20:35:23 +0800

    不想用了

--- nezhahq.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt install curl unzip -y
-
-sudo curl -L https://raw.githubusercontent.com/nezhahq/scripts/main/agent/install.sh -o agent.sh && sudo chmod +x agent.sh && env NZ_SERVER=107.173.19.61:8008 NZ_TLS=false NZ_CLIENT_SECRET=dgNWh5mZsk6zLOjzMuX0BsHCM9JXggYB ./agent.sh

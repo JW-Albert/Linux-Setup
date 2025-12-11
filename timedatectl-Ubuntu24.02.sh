8727bc1608cc8d54fbcb0aefec14a573b30e31e0
JW-Albert <ru04jo30801@gmail.com>
2025-12-11 21:55:55 +0800

    Add clear command in timedatectl-Ubuntu24.02.sh for improved output clarity
    Inserts a clear command before displaying the current time status to enhance user experience by removing previous output. This change aims to improve focus during script execution.

--- timedatectl-Ubuntu24.02.sh
+++ timedatectl-Ubuntu24.02.sh
@@ -41,6 +41,8 @@ sudo timedatectl set-ntp true
 echo "[INFO] Writing system time to hardware clock"
 sudo hwclock --systohc
 
+clear
+
 echo "[INFO] Displaying current time status"
 sudo timedatectl
 sudo hwclock --show

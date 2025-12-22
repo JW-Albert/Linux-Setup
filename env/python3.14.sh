3def5e776a7c51e7e5a63fa42e10e0c5c196a334
Albert Wang <ru04jo30801@gmail.com>
2025-12-22 16:15:36 +0800

    Simplify Python 3.14 installation using PPA
    Updated installation process for Python 3.14 by using the PPA and removing manual download and build steps.

--- env/python3.14.sh
+++ env/python3.14.sh
@@ -4,16 +4,11 @@ clear
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install build-essential libnss3-dev libssl-dev wget libreadline-dev libffi-dev pkg-config zlib1g-dev libncurses5-dev libgdbm-dev wget tar -y
+sudo add-apt-repository ppa:deadsnakes/ppa
 
-cd /opt
-sudo wget https://www.python.org/ftp/python/3.14.2/Python-3.14.2.tgz
+clear
 
-sudo tar -xf Python-3.14.2.tgz
-cd Python-3.14.2/
-sudo ./configure --enable-optimizations
-
-sudo make altinstall
+sudo apt install python3.14 -y
 
 sudo apt install python3-pip python3-venv -y
 

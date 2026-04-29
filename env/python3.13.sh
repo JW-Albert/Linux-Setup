14074746ba6b0baccee8fe9b78e438c75afa7866
Albert Wang <ru04jo30801@gmail.com>
2026-04-29 13:41:43 +0800

    Install Python 3.13 instead of 3.14

---
+++ env/python3.13.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+
+sudo apt update && sudo apt upgrade -y
+
+sudo add-apt-repository ppa:deadsnakes/ppa
+
+clear
+
+sudo apt install python3.13 -y
+
+sudo apt install python3-pip python3-venv -y
+
+clear
+
+echo "Done"
